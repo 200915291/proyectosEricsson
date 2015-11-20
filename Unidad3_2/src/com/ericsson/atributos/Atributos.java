cdf5464c53d115b735558e7d126a90b7bd12e752
Aniras20 <aniras.20@hotmail.com>
2015-11-20 17:22:11 -0600

    commit dia 5

--- Unidad3_2/src/com/ericsson/atributos/Atributos.java
+++ Unidad3_2/src/com/ericsson/atributos/Atributos.java
@@ -2,6 +2,35 @@ package com.ericsson.atributos;
 
 public class Atributos {
 
-	protected Integer height, weight, depth;
+	private Integer height, weight, depth;
+	
+	public Atributos(){
+		
+	}
+
+	public Integer getHeight() {
+		return height;
+	}
+
+	public void setHeight(Integer height) {
+		this.height = height;
+	}
+
+	public Integer getWeight() {
+		return weight;
+	}
+
+	public void setWeight(Integer weight) {
+		this.weight = weight;
+	}
+
+	public Integer getDepth() {
+		return depth;
+	}
+
+	public void setDepth(Integer depth) {
+		this.depth = depth;
+	}
+	
 	
 }
--- Unidad3_2/src/com/ericsson/atributos/MainAtributos.java
+++ Unidad3_2/src/com/ericsson/atributos/MainAtributos.java
@@ -5,13 +5,13 @@ public class MainAtributos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Atributos atr=new Atributos();
-		atr.height=90;
-		atr.weight=78;
-		atr.depth=89;
+		atr.setHeight(90);
+		atr.setWeight(78);
+		atr.setDepth(89);
 		
-		System.out.println("Valor de height: " + atr.height);
-		System.out.println("Valor de weight: " + atr.weight);
-		System.out.println("Valor de depth: " + atr.depth);
+		System.out.println("Valor de height: " + atr.getHeight());
+		System.out.println("Valor de weight: " + atr.getWeight());
+		System.out.println("Valor de depth: " + atr.getDepth());
 
 	}
 
